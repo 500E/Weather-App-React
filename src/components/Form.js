e4e4b3ec4771b386e834e3483cee324669b5b411
Kuba <kubamierzejewski94@gmail.com>
2019-03-04 23:49:48 +0100

    change input field

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,8 +3,8 @@ import React from "react";
 const Form = ({ getWeather }) => {
   return (
     <form onSubmit={getWeather}>
-      <input type="text" name="city" placeholder="City name" />
-      <input type="text" name="country" placeholder="Country name" />
+      <input type="text" name="city" placeholder="City..." />
+      <input type="text" name="country" placeholder="Country..." />
       <button>get weather</button>
     </form>
   );
