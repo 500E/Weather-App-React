c0ac75eb254689ed10873d0f752115f8c356313a
Kuba <kubamierzejewski94@gmail.com>
2019-03-04 23:27:27 +0100

    completed app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Title from "./components/Title";
+import Form from "./components/Form";
+import Weather from "./components/Weather";
+
+const API_KEY = "ae2d920f576e75c5e4c20ca252b782f2";
 
 class App extends Component {
+  state = {
+    temperature: "",
+    city: "",
+    country: "",
+    humidity: "",
+    description: "",
+    icon: "",
+    error: ""
+  };
+
+  getWeather = async e => {
+    e.preventDefault();
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+    const api_call = await fetch(
+      `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`
+    );
+    const weatherInfo = await api_call.json();
+    if (city && country) {
+      this.setState({
+        temperature: weatherInfo.main.temp,
+        city: weatherInfo.name,
+        coutry: weatherInfo.sys.country,
+        humidity: weatherInfo.main.humidity,
+        description: weatherInfo.weather[0].description,
+        icon: `http://openweathermap.org/img/w/${
+          weatherInfo.weather[0].icon
+        }.png`,
+        error: ""
+      });
+    } else {
+      this.setState({
+        temperature: "",
+        city: "",
+        country: "",
+        humidity: "",
+        description: "",
+        icon: "",
+        error:
+          "Please enter the name of the city and the country (for example: Warsaw, PL)."
+      });
+    }
+  };
+
   render() {
+    const {
+      temperature,
+      city,
+      country,
+      humidity,
+      description,
+      icon,
+      error
+    } = this.state;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="app-container">
+        <Title />
+        <Form getWeather={this.getWeather} />
+        <Weather
+          temperature={temperature}
+          city={city}
+          country={country}
+          humidity={humidity}
+          description={description}
+          icon={icon}
+          error={error}
+        />
       </div>
     );
   }
